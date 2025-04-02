0c314fa77a259956510cb49cb09c79ce4c2a97e1
michael <Michaelturvey97@gmail.com>
2025-04-02 11:39:56 +1100

    header info

--- src/userInterface/Header.tsx
+++ src/userInterface/Header.tsx
@@ -1,13 +1,23 @@
+import { GameContext } from "@/src/match/boardGame/Board";
 import { theme } from "@/styles/theme";
 import { Container, Group } from "@mantine/core";
+import { useContext } from "react";
+import Countdown from 'react-countdown';
 import Span from "./Span";
 
 export default function Header({ children }: { children: React.ReactNode }) {
+	const {G, playerData} = useContext(GameContext);
+	const claimedZones = G.zoneData.filter(zone => zone.color === playerData.data.teamColor).length
 	return (
 			<Container style={headerStyles} className="header">
 				<Group justify="center" align="center">
-					<Span size="xs">x neighbourhoods claimed</Span>
-					<Span size="xs">time remaining</Span>
+					<Span size="xs">{claimedZones} neighbourhood{claimedZones === 1 ? "" : "s"} claimed</Span>
+					<Span size="xs">Time remaining: <Countdown 
+					date={new Date(G.endTime || 0)} 
+					renderer={
+						props => <span>{props.hours}h {props.minutes}m {props.seconds}s</span>
+					}
+					/></Span>
 				</Group>
 				{children}
 			</Container>
