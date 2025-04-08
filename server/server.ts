9143833a9180d06e5232225ba233bff779c5fce5
michael <Michaelturvey97@gmail.com>
2025-04-08 10:55:02 +1000

    live server cors

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import { ConnectFour } from '@/scripts/games/connect_four';
 import { Firestore } from 'bgio-firebase';
-import { Origins, Server } from 'boardgame.io/server';
+import { Server } from 'boardgame.io/server';
 import admin from 'firebase-admin';
 
 const database = new Firestore({
@@ -31,7 +31,7 @@ async function buildServer(){
 	//const AllMapsData : Record<string, MatchMapData> = await fetchAllData();
 	const server = Server({
 		games: [ConnectFour],
-		origins: [Origins.LOCALHOST, "http://10.0.0.231:1234", "http://10.0.0.231", "https://nextjs-metrogame--metro-game-474bc.us-central1.hosted.app", "https://otbg-live-test--metro-game-474bc.us-central1.hosted.app"],
+		origins: ["https://metro-game-474bc.web.app"],
 		db: database,
 	});
 
