7cc967cfcdab52240e88d2bbc5e6a08485887757
michael <Michaelturvey97@gmail.com>
2025-04-14 11:38:20 +1000

    firebase config

--- scripts/firebase.ts
+++ scripts/firebase.ts
@@ -6,7 +6,7 @@ import 'firebase/firestore';
 const firebaseConfig = {
 	apiKey: "AIzaSyBQdAxW64FE0ZAOH1oiMhIk_jVQmVROqt4",
 	authDomain: "metro-game-474bc.firebaseapp.com",
-	projectId: "metro-game-474bc",
+	projectId: process.env.FIREBASE,
 	storageBucket: process.env.STORAGE,
 	messagingSenderId: "99015817585",
 	appId: "1:99015817585:web:e596628b1ff97745a3ded0"
