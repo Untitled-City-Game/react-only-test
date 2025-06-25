be49ed931e52cf5c0e5541e1cd00da01c18425c5
tichaelmurvey <Michaelturve97@gmail.com>
2025-06-25 20:57:54 +1000

    src deleted

--- src/userInterface/Span.tsx
+++
@@ -1,4 +0,0 @@
-import { Text, TextProps } from "@mantine/core";
-export default function Span({ children, ...rest }: TextProps & { children: React.ReactNode }) {
-	return <Text span {...rest}>{children}</Text>;
-}
