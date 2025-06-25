0102b7147507b17d47865aad6056a377647363d4
tichaelmurvey <Michaelturve97@gmail.com>
2025-06-25 20:04:38 +1000

    server staging settings

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,6 @@
-import { cities } from '@/scripts/consts';
 import { ConnectFour } from '@/scripts/games/connect_four/connect_four';
-import { MatchMapData } from '@/scripts/types';
-import { fetchMapData } from '@scripts/fetchMapData';
 import { Firestore } from 'bgio-firebase';
-import { FlatFile, Origins, Server } from 'boardgame.io/server';
+import { Origins, Server } from 'boardgame.io/server';
 import admin from 'firebase-admin';
 
 const database = new Firestore({
@@ -14,29 +11,12 @@ const database = new Firestore({
   });
   
 
-
-async function fetchAllData(){
-	const allData : Record<string, MatchMapData> = {};
-	for (const city of cities){
-		const mapData = await fetchMapData(city).catch(e => console.error(e));
-		if (!mapData) continue;
-		console.log("fetched data for city", city);
-		allData[city] = mapData;
-		continue;
-	}
-	return allData;
-}
-
-
 async function buildServer(){
 	console.log("building server", process.env.GAME_ADDRESS, process.env.GAME_SERVER);
-	//const AllMapsData : Record<string, MatchMapData> = await fetchAllData();
 	const server = Server({
 		games: [ConnectFour],
 		origins: [Origins.LOCALHOST,  process.env.GAME_ADDRESS || false],
-		db: new FlatFile({
-			dir: process.cwd() + '/server/db',
-		})
+		db: database
 	});
 
 	server.router.get('/hello', (ctx) => {
@@ -47,7 +27,7 @@ async function buildServer(){
 		const mapData = await fetch(`https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=${ctx.params.citycode}`);
 		ctx.body = await mapData.text();
 	  });
-	const PORT = parseInt(process.env.PORT || "8000");
+	const PORT = parseInt(process.env.PORT || "8080");
 	server.run(PORT, () => console.log("server running..."));
 }
 
