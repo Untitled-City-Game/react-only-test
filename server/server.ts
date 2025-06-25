e60ce8c6db6d28944c84211adf98124e60960a30
tichaelmurvey <Michaelturve97@gmail.com>
2025-06-25 19:50:02 +1000

    server updates

--- server/server.ts
+++ server/server.ts
@@ -33,7 +33,7 @@ async function buildServer(){
 	//const AllMapsData : Record<string, MatchMapData> = await fetchAllData();
 	const server = Server({
 		games: [ConnectFour],
-		origins: [Origins.LOCALHOST,  process.env.GAME_ADDRESS || false, "*"],
+		origins: [Origins.LOCALHOST,  process.env.GAME_ADDRESS || false],
 		db: new FlatFile({
 			dir: process.cwd() + '/server/db',
 		})
