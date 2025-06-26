99fe85ebc7dec3c5e27a28123410bca7762d5cf2
tichaelmurvey <Michaelturve97@gmail.com>
2025-06-26 15:01:28 +1000

    test server

--- server/server.ts
+++ server/server.ts
@@ -1,34 +1,31 @@
-import { ConnectFour } from '@/scripts/games/connect_four/connect_four';
-import { Firestore } from 'bgio-firebase';
 import { Origins, Server } from 'boardgame.io/server';
-import admin from 'firebase-admin';
 
-const database = new Firestore({
-	app: process.env.FIREBASE,
-	config: {
-		credential: admin.credential.applicationDefault(),
-		databaseURL: `https://${process.env.FIREBASE}.firebaseio.com`,
-	},
-  });
+// const database = new Firestore({
+// 	app: process.env.FIREBASE,
+// 	config: {
+// 		credential: admin.credential.applicationDefault(),
+// 		databaseURL: `https://${process.env.FIREBASE}.firebaseio.com`,
+// 	},
+//   });
   
 
 async function buildServer(){
 	console.log("building server", process.env.GAME_ADDRESS, process.env.GAME_SERVER);
 	//const AllMapsData : Record<string, MatchMapData> = await fetchAllData();
 	const server = Server({
-		games: [ConnectFour],
+		games: [],
 		origins: [process.env.GAME_ADDRESS ==='localhost' && Origins.LOCALHOST || process.env.GAME_ADDRESS || false],
-		db: database,
+		// db: database,
 	});
 
 	server.router.get('/hello', (ctx) => {
 		ctx.body = 'Hello ee!';
 	  });
-	server.router.get('/map-data/:citycode', async (ctx) => {
-		console.log("getting map data for city", ctx.params.citycode);
-		const mapData = await fetch(`https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=${ctx.params.citycode}`);
-		ctx.body = await mapData.text();
-	  });
+	// server.router.get('/map-data/:citycode', async (ctx) => {
+	// 	console.log("getting map data for city", ctx.params.citycode);
+	// 	const mapData = await fetch(`https://www.google.com/maps/d/u/0/kml?forcekml=1&mid=${ctx.params.citycode}`);
+	// 	ctx.body = await mapData.text();
+	//   });
 	const PORT = parseInt(process.env.PORT || "8080");
 	server.run(PORT, () => console.log("server running..."));
 }
