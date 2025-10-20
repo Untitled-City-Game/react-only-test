a754e640b90f8f7a213706b71d549ca909e54c4f
tichaelmurvey <michaelturvey97@gmail.com>
2025-10-20 15:57:22 -0400

    updated socket endpoint

--- scripts/socket.ts
+++ scripts/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'https://outside.funwebsite.fun/teamlocations/' : 'http://localhost:3000/';
+const URL = process.env.NODE_ENV === 'production' ? 'https://outside.funwebsite.fun/' : 'http://localhost:3000/';
 //const URL = 'https://outside.funwebsite.fun/'
 //const URL = 'http://localhost:3000/'
 export const socket = io(URL, {
