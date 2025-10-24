dc63ae45a688b31e5ca93280ed7c3106ab0b073d
tichaelmurvey <michaelturvey97@gmail.com>
2025-10-24 19:39:23 -0400

    seperate location server

---
+++ server/locationServer.ts
@@ -0,0 +1,37 @@
+import { createServer } from 'node:http';
+import * as socketIo from 'socket.io';
+
+const httpServer = createServer()
+
+const io = new socketIo.Server(httpServer, {
+	  path: process.env.LOCATION_SERVER_PATH,
+	  connectionStateRecovery: {
+		// the backup duration of the sessions and the packets
+		maxDisconnectionDuration: 2 * 60 * 1000,
+		// whether to skip middlewares upon successful recovery
+		skipMiddlewares: true,
+  		},
+	  cors: {
+		origin: [process.env.LAN_ADDRESS || false, process.env.GAME_ADDRESS || false],
+		methods: ["GET", "POST"]
+	 }
+})
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+	console.log('user disconnected');
+  });
+  socket.on("foo", (body) => {
+	console.log("someone said foo " + body);
+	io.emit("foo", body)
+  })
+  socket.on("locationUpdate", (body) => {
+	//console.log("someone updated the location", body);
+	socket.broadcast.emit("locationUpdate", body)
+  })
+});
+
+io.listen(parseInt(process.env.LOCATION_SERVER_PORT || "3000"));
+
