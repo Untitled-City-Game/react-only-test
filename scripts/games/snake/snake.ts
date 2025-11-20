dff32480d5e592c4e12e35b3490d891545fb7a2d
tichaelmurvey <michaelturvey97@gmail.com>
2025-11-20 15:21:16 -0500

    added scripts

---
+++ scripts/games/snake/snake.ts
@@ -0,0 +1,59 @@
+import { addTeamPhoto } from "@/scripts/games/shared_moves/addTeamPhoto";
+import { endGame, startGame } from "@/scripts/games/shared_moves/manageGame";
+import { sharedMoves } from "@/scripts/games/shared_moves/sharedMoves";
+import { completeChallengeAndEatFruit } from "@/scripts/games/snake/moves/eatFruit";
+import { addSegment } from "@/scripts/games/snake/moves/manageSnakeBody";
+import {updateFruit} from "@/scripts/games/snake/moves/spawnFruit";
+import { snakeGameSetup } from "@/scripts/games/snake/setup";
+import { snakePlayerSetup } from "@/scripts/games/snake/snakePlayerSetup";
+import { SnakeGameState, SnakeGameSetupData } from "@/scripts/games/snake/types";
+import customUndoTemplate from "@/scripts/games/undo";
+import { MoveContext, StripContext } from "@/scripts/types/types";
+import { Game } from "boardgame.io";
+
+
+const customUndo = (context : MoveContext<SnakeGameState>) => {
+	customUndoTemplate(context)
+}
+const playStateMoves_Snake = {
+	endGame,
+	customUndo,
+	playerSetup : snakePlayerSetup,
+	addSegment,
+	updateFruit,
+	completeChallengeAndEatFruit,
+	addTeamPhoto,
+};
+export type PlayStateMoves_Snake = StripContext<typeof playStateMoves_Snake>;
+
+export const Snake: Game<SnakeGameState> = {
+	name: `snake`,
+	setup: ({ ctx }, setupData : SnakeGameSetupData) => snakeGameSetup(ctx, setupData),
+	endIf: ({ G }) => {
+		console.log("gameover check", G.gameOver);
+		return G.gameOver ? "Game ended" : null;
+	},
+	
+	moves: {
+		startGame : (args: any) => startGame<SnakeGameState>(args),
+		playerSetup: snakePlayerSetup,
+	},
+	
+	turn: {
+		onBegin: ({ events }) => {
+			events.setActivePlayers({ all: "join" });
+		},
+		stages: {
+			join: {
+				moves: {
+					playerSetup: snakePlayerSetup,
+					startGame : (args: any) => startGame<SnakeGameState>(args),
+					...sharedMoves
+				},
+			},
+			play: {
+				moves: playStateMoves_Snake,
+			},
+		},
+	},
+}
