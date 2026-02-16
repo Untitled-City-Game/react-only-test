da749433d3f4b8a42ca88352ee2e21400791453a
SteamOS User <michaelturvey97@gmail.com>
2026-02-16 16:04:21 -0500

    made connect 4 orange

--- scripts/consts.ts
+++ scripts/consts.ts
@@ -9,109 +9,109 @@ export const ACCURACY_MAX = 300;
 export const numPlayers = 6;
 export const gameTimeMinutes = 180;
 export const cities = [
-	"melbourne",
-	"montreal",
-	"montreal_mini",
-	"london",
-	"nyc"
+    "melbourne",
+    "montreal",
+    "montreal_mini",
+    "london",
+    "nyc"
 ] as const;
 
 export const maps = {
-	melbourne: {
-		name: "Melbourne",
-		code: "melbourne",
-		kml_live_id: "1h1mLlEU1PRRbiF9eusZUplFhg7wjCaU",
-	},
-	montreal: {
-		name: "Montreal",
-		code: "montreal",
-		kml_live_id: "1bliTnqrqX9A6txnta7C27Em0PJ3phzE",
-	},
-	montreal_mini: {
-		name: "Montreal mini",
-		code: "montreal_mini",
-		kml_live_id: "1RCl4vAXoYFF8HCwX7wTAVlgSs765ZJk"
-	},
-	london: {
-		name: "London",
-		code: "london",
-		kml_live_id: "1DWUN7rKm1FUFr0BiKme7f0NpqegZWos",
-	},
-	nyc: {
-		name: "Manhattan",
-		code: "nyc",
-		kml_live_id: "1L42PLDup2h_AiMSNTT1hAdBoyE5P6Ig"
-	}
+    melbourne: {
+        name: "Melbourne",
+        code: "melbourne",
+        kml_live_id: "1h1mLlEU1PRRbiF9eusZUplFhg7wjCaU",
+    },
+    montreal: {
+        name: "Montreal",
+        code: "montreal",
+        kml_live_id: "1bliTnqrqX9A6txnta7C27Em0PJ3phzE",
+    },
+    montreal_mini: {
+        name: "Montreal mini",
+        code: "montreal_mini",
+        kml_live_id: "1RCl4vAXoYFF8HCwX7wTAVlgSs765ZJk"
+    },
+    london: {
+        name: "London",
+        code: "london",
+        kml_live_id: "1DWUN7rKm1FUFr0BiKme7f0NpqegZWos",
+    },
+    nyc: {
+        name: "Manhattan",
+        code: "nyc",
+        kml_live_id: "1L42PLDup2h_AiMSNTT1hAdBoyE5P6Ig"
+    }
 }
 
 export const gameLocationCenters = {
-	melbourne: {
-		lat: -37.8136,
-		lng: 144.9631
-	},
-	montreal: {
-		lat:  45.5070113731678,
-		lng: -73.57570671343632,
-	},
-	london: {
-		lat:  51.5073509,
-		lng: -0.1277583,
-	},
-	montreal_mini: {
-				lat:  45.5070113731678,
-		lng: -73.57570671343632,
-	},
-	nyc : {
-		lat: 40.74807572237176,
-		lng: -73.98590688638745
-	}
+    melbourne: {
+        lat: -37.8136,
+        lng: 144.9631
+    },
+    montreal: {
+        lat: 45.5070113731678,
+        lng: -73.57570671343632,
+    },
+    london: {
+        lat: 51.5073509,
+        lng: -0.1277583,
+    },
+    montreal_mini: {
+        lat: 45.5070113731678,
+        lng: -73.57570671343632,
+    },
+    nyc: {
+        lat: 40.74807572237176,
+        lng: -73.98590688638745
+    }
 }
 
 //games
 export const games = [
-	{
-		name: "Connect Four",
-		code: "connect_four",
-		active: true,
-		description: "Connect four neighbourhoods in your city before the other team!",
-		playerMin: 4,
-		playerMax: 8,
-		color: "purple",
-		icon: MdGrid4X4
-	},
-	{
-		name: "Snake",
-		code: "snake",
-		active: true,
-		playerMin: 4,
-		playerMax: 8,
-		description: "Collect the fruit to grow your snake. Wrap around the other team to win!",
-		color: "green",
-		icon: VscSnake
-	},
-	{
-		name: "Tag",
-		code: "tag",
-		active: false,
-		playerMin: 6,
-		playerMax: 30,
-		description: "Explore the city and dodge the taggers - or chase your friends!",
-		color: "purple",
-		icon: PiPersonSimpleRunBold 
-	},
-	{
-		name: "Battleship",
-		code: "battleship",
-		active: false,
-		playerMin: 4,
-		playerMax: 8,
-		description: "Find and destroy the other team before they find you!",
-		color: "water",
-		icon: GiRadarSweep
-	},
+    {
+        name: "Connect Four",
+        code: "connect_four",
+        active: true,
+        description: "Connect four neighbourhoods in your city before the other team!",
+        playerMin: 4,
+        playerMax: 8,
+        color: "orange",
+        icon: MdGrid4X4
+    },
+    {
+        name: "Snake",
+        code: "snake",
+        active: true,
+        playerMin: 4,
+        playerMax: 8,
+        description: "Collect the fruit to grow your snake. Wrap around the other team to win!",
+        color: "green",
+        icon: VscSnake
+    },
+    {
+        name: "Tag",
+        code: "tag",
+        active: false,
+        playerMin: 6,
+        playerMax: 30,
+        description: "Explore the city and dodge the taggers - or chase your friends!",
+        color: "purple",
+        icon: PiPersonSimpleRunBold
+    },
+    {
+        name: "Battleship",
+        code: "battleship",
+        active: false,
+        playerMin: 4,
+        playerMax: 8,
+        description: "Find and destroy the other team before they find you!",
+        color: "water",
+        icon: GiRadarSweep
+    },
 ]
 
-   //Montreal
+//Montreal
 
 //   const center = {
 // 	lat:  45.529819917244254,
