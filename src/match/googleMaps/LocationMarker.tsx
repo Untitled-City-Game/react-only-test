925ef8243bc222eb2a8e9781212ec0720a1dea5a
Michael Turvey <michaelturvey97@gmail.com>
2026-05-09 00:22:17 +1000

    default team image

--- src/match/googleMaps/LocationMarker.tsx
+++ src/match/googleMaps/LocationMarker.tsx
@@ -13,60 +13,61 @@ import useEase from "@/scripts/ease";
 
 const iconSize = 30
 
-export default function LocationMarker({ position, accuracy, color, invalid, expired }: 
-	{ 	position: google.maps.LatLngLiteral, 
-		accuracy?: number, 
-		color: MatchTeamColor, 
-		invalid?: boolean,
-		expired?: boolean
+export default function LocationMarker({ position, accuracy, color, invalid, expired }:
+    {
+        position: google.maps.LatLngLiteral,
+        accuracy?: number,
+        color: MatchTeamColor,
+        invalid?: boolean,
+        expired?: boolean
 
-	}) {
-	const { G: gameData } = useContext(GameContext)
-	const theme = useMantineTheme();
-	const accurate = ((accuracy??ACCURACY_MAX) < ACCURACY_MAX) && !invalid
-	//update my location with location server
-	const teamImgStyle : React.CSSProperties = {
-		height: `${iconSize}px`,
-		width: `${iconSize}px`,
-		objectFit: "cover",
-		borderRadius: "50%",
-		position: "absolute",
-		bottom: `${iconSize * 0.75}px`,
-		left: `${-iconSize / 2}px`,
-		zIndex: "3",
-	}
-	const filteredAccuracy = accuracy ? accuracy > ACCURACY_MAX ? 0 : accuracy : 0;
-	const accuracyRadius = useEase(filteredAccuracy, 200);
-	const positionEased = {lat : useEase(position.lat, 200, 13), lng : useEase(position.lng, 200, 13)}
-	return (
-		<AdvancedMarker position={positionEased}>
-			{/* <P fz="xs">accurate: {accurate ? 'true' : 'false'}</P>
+    }) {
+    const { G: gameData } = useContext(GameContext)
+    const theme = useMantineTheme();
+    const accurate = ((accuracy ?? ACCURACY_MAX) < ACCURACY_MAX) && !invalid
+    //update my location with location server
+    const teamImgStyle: React.CSSProperties = {
+        height: `${iconSize}px`,
+        width: `${iconSize}px`,
+        objectFit: "cover",
+        borderRadius: "50%",
+        position: "absolute",
+        bottom: `${iconSize * 0.75}px`,
+        left: `${-iconSize / 2}px`,
+        zIndex: "3",
+    }
+    const filteredAccuracy = accuracy ? accuracy > ACCURACY_MAX ? 0 : accuracy : 0;
+    const accuracyRadius = useEase(filteredAccuracy, 200);
+    const positionEased = { lat: useEase(position.lat, 200, 13), lng: useEase(position.lng, 200, 13) }
+    return (
+        <AdvancedMarker position={positionEased}>
+            {/* <P fz="xs">accurate: {accurate ? 'true' : 'false'}</P>
 			<P fz="xs">expired: {expired ? 'true' : 'false'}</P>
 			<P fz="xs">invalid: {invalid ? 'true' : 'false'}</P> */}
-			<IoLocationSharp
-				fill={accurate ? theme.colors[color][6] : theme.colors[color][9]}
-				stroke="white"
-				strokeWidth={10}
-				strokeOpacity={1}
-				size={`${iconSize * 2}px`}
-				style={{
-					position: "absolute",
-					bottom: "0px",
-					left: `${-iconSize}px`,
-					zIndex: "2",
-				}}
-			/>
-			
-			<img style={teamImgStyle} src={gameData.teamPhotoURLs[color] ?? "/snakemarker.png"} />
-			{accurate ? <Circle 
-				center={positionEased} 
-				radius={accuracyRadius}
-				strokeColor={color} 
-				fillColor={color} 
-				strokeOpacity={0.3} 
-				fillOpacity={0.1} 
-				clickable={false}
-			/> : <FaQuestionCircle className="location-missing" size="4rem" opacity={0.3} style={{transform: "translate(-50%, 50%)", zIndex: "0"}} />}
-		</AdvancedMarker>
-	)
+            <IoLocationSharp
+                fill={accurate ? theme.colors[color][6] : theme.colors[color][9]}
+                stroke="white"
+                strokeWidth={10}
+                strokeOpacity={1}
+                size={`${iconSize * 2}px`}
+                style={{
+                    position: "absolute",
+                    bottom: "0px",
+                    left: `${-iconSize}px`,
+                    zIndex: "2",
+                }}
+            />
+
+            <img style={teamImgStyle} src={gameData.teamPhotoURLs[color] ?? "/default-team.png"} />
+            {accurate ? <Circle
+                center={positionEased}
+                radius={accuracyRadius}
+                strokeColor={color}
+                fillColor={color}
+                strokeOpacity={0.3}
+                fillOpacity={0.1}
+                clickable={false}
+            /> : <FaQuestionCircle className="location-missing" size="4rem" opacity={0.3} style={{ transform: "translate(-50%, 50%)", zIndex: "0" }} />}
+        </AdvancedMarker>
+    )
 }
